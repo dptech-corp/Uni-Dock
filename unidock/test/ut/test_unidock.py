b8be4f336ebfdd5ba0d0b5cccd0fac3bae39f4d2
Yannan <yuanyn@dp.tech>
2023-08-22 10:38:17 +0800

    unidock test use image

---
+++ unidock/test/ut/test_unidock.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+from pathlib import Path
+import uuid
+import subprocess
+import unittest as ut
+
+
+class TestUniDock(ut.TestCase):
+    def setUp(self):
+        self.workdir = Path(f"/tmp/{uuid.uuid4()}")
+        self.workdir.mkdir(parents=True, exist_ok=True)
+        self.receptor = os.path.join(os.path.dirname(__file__), "..", "receptor", "1iep_receptor.pdbqt")
+        self.ligand = os.path.join(os.path.dirname(__file__), "..", "ligands", "1iep_ligand.pdbqt")
+        self.pocket = [15.19, 53.903, 16.917, 20, 20, 20]
+
+    def tearDown(self):
+        shutil.rmtree(self.workdir, ignore_errors=True)
+
+    def test_unidock(self):
+        cmd = f"unidock --receptor {self.receptor} --gpu_batch {self.ligand} --dir {self.workdir} \
+            --center_x {self.pocket[0]} --center_y {self.pocket[1]} --center_z {self.pocket[2]} \
+            --size_x {self.pocket[3]} --size_y {self.pocket[4]} --size_z {self.pocket[5]} \
+            --scoring vina --search_mode fast --num_modes 1 --seed 181129"
+        resp = subprocess.run(cmd, shell=True, 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            encoding="utf-8")
+        print(resp.stdout)
+        if resp.stderr:
+            print(resp.stderr)
+        result_ligand = os.path.join(self.workdir, "1iep_ligand_out.pdbqt")
+        self.assertTrue(os.path.exists(result_ligand))
+        score_line = ""
+        with open(result_ligand, "r") as f:
+            for line in f.readlines():
+                if line.startswith("REMARK VINA RESULT:"):
+                    score_line = line.strip("\n")
+                    break
+        self.assertNotEqual(score_line, "")
+        score = float([e for e in score_line[len("REMARK VINA RESULT:"):].split(" ") if e!=""][0])
+        self.assertEqual(score, -10.33)
+
+
+if __name__ == "__main__":
+    ut.main()
