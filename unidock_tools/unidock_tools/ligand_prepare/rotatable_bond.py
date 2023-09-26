ab8ae7a7a1a72eba53fb56e2b7600434b70e71b9
Hong-Rui Lin <mick3101@gmail.com>
2023-09-26 11:36:30 +0800

    Update rotatable_bond.py

--- unidock_tools/unidock_tools/ligand_prepare/rotatable_bond.py
+++ unidock_tools/unidock_tools/ligand_prepare/rotatable_bond.py
@@ -8,8 +8,10 @@ class RotatableBond(object):
                  max_breaks=4):
 
         self.rotatable_bond_smarts = '[!$(*#*)&!D1]-&!@[!$(*#*)&!D1]'
+        self.amide_bond_smarts = '[C&$(C=O)]-[N&$(NC=O);v3;H1,H2;0]'
         self.conjugate_bond_smarts = '*=*[*]=,#,:[*]'
         self.rotatable_bond_pattern = Chem.MolFromSmarts(self.rotatable_bond_smarts)
+        self.amide_bond_pattern = Chem.MolFromSmarts(self.amide_bond_smarts)
         self.conjugate_bond_pattern = Chem.MolFromSmarts(self.conjugate_bond_smarts)
 
         self.min_macrocycle_size = min_macrocycle_size
@@ -19,4 +21,13 @@ class RotatableBond(object):
 
     def identify_rotatable_bonds(self, mol):
         default_rotatable_bond_info_list = list(mol.GetSubstructMatches(self.rotatable_bond_pattern))
+        amide_rotatable_bond_info_list = list(mol.GetSubstructMatches(self.amide_bond_pattern))
+
+        for amide_rotatable_bond_info in amide_rotatable_bond_info_list:
+            amide_rotatable_bond_info_reversed = tuple(reversed(amide_rotatable_bond_info))
+            if amide_rotatable_bond_info in default_rotatable_bond_info_list:
+                default_rotatable_bond_info_list.remove(amide_rotatable_bond_info)
+            elif amide_rotatable_bond_info_reversed in default_rotatable_bond_info_list:
+                default_rotatable_bond_info_list.remove(amide_rotatable_bond_info_reversed)
+
         return default_rotatable_bond_info_list
