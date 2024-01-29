a309df391e8a61c9f4fcf30aedcc752f89749b75
dp-yuanyn <yuanyn@dp.tech>
2024-01-30 02:12:47 +0800

    fix ut

--- unidock/test/ut/test_unidock.py
+++ unidock/test/ut/test_unidock.py
@@ -17,16 +17,17 @@ class TestUniDock(ut.TestCase):
     def tearDown(self):
         shutil.rmtree(self.workdir, ignore_errors=True)
 
-    def test_unidock(self):
+    def test_unidock_vina(self):
         cmd = f"unidock --receptor {self.receptor} --gpu_batch {self.ligand} --dir {self.workdir} \
             --center_x {self.pocket[0]} --center_y {self.pocket[1]} --center_z {self.pocket[2]} \
             --size_x {self.pocket[3]} --size_y {self.pocket[4]} --size_z {self.pocket[5]} \
             --scoring vina --search_mode fast --num_modes 1 --seed 181129"
         resp = subprocess.run(cmd, shell=True, 
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            encoding="utf-8")
+                              capture_output=True,
+                              encoding="utf-8", 
+                              cwd=self.workdir)
         print(resp.stdout)
-        if resp.stderr:
+        if resp.returncode != 0:
             print(resp.stderr)
         result_ligand = os.path.join(self.workdir, "1iep_ligand_out.pdbqt")
         self.assertTrue(os.path.exists(result_ligand))
@@ -38,8 +39,31 @@ class TestUniDock(ut.TestCase):
                     break
         self.assertNotEqual(score_line, "")
         score = float([e for e in score_line[len("REMARK VINA RESULT:"):].split(" ") if e!=""][0])
-        self.assertEqual(score, -10.33)
+        self.assertTrue(-12 <= score <= -5)
 
+    def test_unidock_vinardo(self):
+        cmd = f"unidock --receptor {self.receptor} --gpu_batch {self.ligand} --dir {self.workdir} \
+            --center_x {self.pocket[0]} --center_y {self.pocket[1]} --center_z {self.pocket[2]} \
+            --size_x {self.pocket[3]} --size_y {self.pocket[4]} --size_z {self.pocket[5]} \
+            --scoring vinardo --search_mode fast --num_modes 1 --seed 181129"
+        resp = subprocess.run(cmd, shell=True, 
+                              capture_output=True,
+                              encoding="utf-8", 
+                              cwd=self.workdir)
+        print(resp.stdout)
+        if resp.returncode != 0:
+            print(resp.stderr)
+        result_ligand = os.path.join(self.workdir, "1iep_ligand_out.pdbqt")
+        self.assertTrue(os.path.exists(result_ligand))
+        score_line = ""
+        with open(result_ligand, "r") as f:
+            for line in f.readlines():
+                if line.startswith("REMARK VINA RESULT:"):
+                    score_line = line.strip("\n")
+                    break
+        self.assertNotEqual(score_line, "")
+        score = float([e for e in score_line[len("REMARK VINA RESULT:"):].split(" ") if e!=""][0])
+        self.assertTrue(-12 <= score <= -5)
 
 if __name__ == "__main__":
     ut.main()
